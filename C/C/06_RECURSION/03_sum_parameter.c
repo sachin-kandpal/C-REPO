39ce7d950be33540774c76b09bb0f40c6d7237a6
sachin-Kandpal <sachinkandpal0@gmail.com>
2026-02-09 16:10:12 +0530

    new program on recursion

--- C/C/06_RECURSION/03_sum_parameter.c
+++ C/C/06_RECURSION/03_sum_parameter.c
@@ -1,14 +1,33 @@
+// // method 1
+// #include<stdio.h>
+// void sum(int n,int s){
+//     if(n==0){
+//         printf("%d",s);
+//         return;
+//     }
+//     sum(n-1,s+n);
+//     return;
+
+// }
+// int main(){
+//     int n;
+//     printf("enter num - ");
+//     scanf("%d",&n);
+//     sum(n,0);
+//     return 0;
+// }
+
+//  method -2
 #include<stdio.h>
-void incresing(int n){
-    if(n==0) return;
-    incresing(n-1);
-    printf("%d\n",n);
-    return;
+int sum(int n){
+    if(n==1 || n==0) return 1;
+    return n + sum(n-1);
 }
 int main(){
     int n;
     printf("enter num - ");
     scanf("%d",&n);
-    incresing(n);
+    int fact = sum(n);
+    printf("%d",fact);
     return 0;
 }
