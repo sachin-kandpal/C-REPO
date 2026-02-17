37e506162466ba8f8f4ee8bee9f9a4f6489ffdd7
sachin-Kandpal <sachinkandpal0@gmail.com>
2026-02-17 21:00:57 +0530

    Added new programs

---
+++ C/C/06_RECURSION/07_power_log_rec.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int power(int a, int b){
+    if(b == 0) return 1;
+
+    int half = power(a, b/2);
+
+    if(b % 2 == 0){
+        return half * half;
+    }
+    else{
+        return a * half * half;
+    }
+}
+
+int main(){
+    int a;
+    printf("enter base - ");
+    scanf("%d",&a);
+
+    int b;
+    printf("enter power - ");
+    scanf("%d",&b);
+
+    int p = power(a,b);
+    printf("%d",p);
+
+    return 0;
+}
